885ebb09b99ac848b54e4021c15435b81dd9f3b0
Pierre Gonzalez <pierre.gonzalez.medoc@gmail.com>
2021-05-16 23:39:09 +0200

    Affichage User Cards

--- SuperCardGame/src/main/java/com/sp/rest/CardRestCrt.java
+++ SuperCardGame/src/main/java/com/sp/rest/CardRestCrt.java
@@ -2,7 +2,6 @@
 package com.sp.rest;
 
 import java.util.ArrayList;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CookieValue;
--- SuperCardGame/src/main/java/com/sp/service/CardService.java
+++ SuperCardGame/src/main/java/com/sp/service/CardService.java
@@ -1,7 +1,6 @@
 package com.sp.service;
 
 import java.util.Optional;
-import java.util.function.Consumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
