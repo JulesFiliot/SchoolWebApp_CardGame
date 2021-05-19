573b12556181fbf3b1d5660acadb9a40ed9aab6e
MatthieuGourdon <matthieu.gourdon@cpe.fr>
2021-05-19 16:30:10 +0200

    logout hub

--- SuperCardGame/src/main/java/com/sp/rest/LoginRestCrt.java
+++ SuperCardGame/src/main/java/com/sp/rest/LoginRestCrt.java
@@ -52,7 +52,15 @@ import com.sp.service.UserService;
       @RequestMapping("/logout")
       public void logout(HttpServletResponse response) {
 		  Cookie cookie = new Cookie("id", "0");
+  		  System.out.println("kiku");
+
 		  response.addCookie(cookie);
+		  try {
+				response.sendRedirect("index.html");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		  return;
       }
 
