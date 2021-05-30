4ae4bf5032f7f4ef629c8db97fcb9d79054d8387
Jules FILIOT <jules.filiot@cpe.fr>
2021-05-30 22:45:14 +0200

    merge commit

--- auth/src/main/java/com/scg/auth/rest/AuthRestCrt.java
+++ auth/src/main/java/com/scg/auth/rest/AuthRestCrt.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.swing.text.html.Option;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.sp.service.UserService;
+//import com.sp.service.UserService;
 import com.scg.auth.service.AuthService;
 //import com.sp.service.UserService;
 
@@ -39,11 +40,13 @@ public class AuthRestCrt {
     	
     	String reqUrl = "http://127.0.0.1:8081/getUserId";
         RestTemplate restTemplate = new RestTemplate();
-		//int uId = restTemplate.getForObjetc(reqUrl, Object[].class);
+		ResponseEntity<Object[]> id = restTemplate.getForEntity(reqUrl, Object[].class);
     	
-  	  int id = uService.checkUser(username, password);
+		System.out.println(id);
+		
+  	  //int id = uService.checkUser(username, password);
   	  
-  	  if (id!=0) {
+  	 /* if (id!=0) {
   		  Cookie cookie = new Cookie("id", String.valueOf(id));
 
   		  //add cookie to response
@@ -54,7 +57,8 @@ public class AuthRestCrt {
 				e.printStackTrace();
 			}
   		  return ;
-  		  }
+  		  }*/
+		return;
     }
     
     @RequestMapping("/logout")
@@ -97,8 +101,9 @@ public class AuthRestCrt {
   //	    return "Hey! My id is " + id;
   //	}
     
+    
     @RequestMapping("/getCurrentUserId")
     public String readCookie(@CookieValue(value = "id", defaultValue = "0") String id) {
   	  return "The id of the current user is "+uService.readCookie(id);
-    }    
+    }
 }
