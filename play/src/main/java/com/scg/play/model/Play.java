2fe6629bb9050d1b8bcb9580b69b10f3041c9cee
Pierre Gonzalez <pierre.gonzalez.medoc@gmail.com>
2021-05-31 16:37:39 +0200

    Model Play & Repo

---
+++ play/src/main/java/com/scg/play/model/Play.java
@@ -0,0 +1,53 @@
+package com.scg.play.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Play {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	private String name;
+	private Integer pari;
+	private Integer IdP1;
+	private Integer IdP2;
+	
+	
+	public Play(String name, Integer pari) {
+		super();
+		this.name = name;
+		this.pari = pari;
+		this.IdP1 = 0;
+		this.IdP2 = 0;
+	}
+	
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public int getPari() {
+		return this.pari;
+	}
+	
+	public void setPari(int pari) {
+		this.pari = pari;
+	}
+}
