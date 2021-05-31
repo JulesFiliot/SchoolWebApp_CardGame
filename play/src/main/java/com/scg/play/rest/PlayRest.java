f0a99ec5fe7a7922fea97ad53561331bf41913aa
Pierre Gonzalez <pierre.gonzalez.medoc@gmail.com>
2021-05-31 18:27:48 +0200

    PlayService premieres methodes

--- play/src/main/java/com/scg/play/rest/PlayRest.java
+++ play/src/main/java/com/scg/play/rest/PlayRest.java
@@ -1,12 +1,23 @@
 package com.scg.play.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.scg.play.model.Play;
 import com.scg.play.service.PlayService;
 
 public class PlayRest {
 
 	@Autowired
 	PlayService pService;
+	
+	
+    @RequestMapping(method=RequestMethod.POST,value="/addPlay")
+    public void addPlay(@RequestBody Play p) {
+        pService.addPlay(p);
+        return;
+    }
 	
 }
--- play/src/main/java/com/scg/play/service/PlayService.java
+++ play/src/main/java/com/scg/play/service/PlayService.java
@@ -1,7 +1,39 @@
 package com.scg.play.service;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
+import com.scg.play.model.Play;
+import com.scg.play.repository.PlayRepository;
+
+
 public class PlayService {
 
+	@Autowired
+	PlayRepository pRepository;
 	
+	public void addPlay(Play p) {
+		Play createdPlay = pRepository.save(p);
+		System.out.println(createdPlay);
+		/*
+        String reqCard = "http://127.0.0.1:8081/generateCards/"+u.getId();
+        RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getForEntity(reqCard, Object[].class);
+		*/	
+	}
+	
+	public ArrayList<Play> getAllPlay() {
+		ArrayList<Play> ListPlay = new ArrayList<Play>();
+		Iterable<Play> allPlay =pRepository.findAll();
+		Iterator<Play> iterator = allPlay.iterator();
+		while(iterator.hasNext()) {
+		    Play it = iterator.next();
+		    ListPlay.add(it);
+		}
+		return ListPlay;
+ 	}
 	
 }
