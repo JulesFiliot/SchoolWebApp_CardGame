54d485348bba99e7b15b21177f8d84e8b48c8216
MatthieuGourdon <matthieu.gourdon@cpe.fr>
2021-06-01 01:18:16 +0200

    uId change

--- market/src/main/java/com/scg/market/rest/MarketRest.java
+++ market/src/main/java/com/scg/market/rest/MarketRest.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.swing.text.html.Option;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.scg.market.service.MarketService;
@@ -29,12 +31,20 @@ public class MarketRest {
     MarketService mService;
     
     @RequestMapping(method=RequestMethod.GET,value="/buyCard/{cId}")
-    public void buyCard (@CookieValue (value = "id", defaultValue = "0") String uId, @PathVariable String cId) {
-		  mService.buyCard(uId,cId);
+    public void buyCard (@PathVariable String cId) {
+    	String reqUrl = "http://127.0.0.1:8083/getAuthId";
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<Integer> bodyIu = restTemplate.getForEntity(reqUrl, Integer.class);
+        Integer uId = bodyIu.getBody();
+    	mService.buyCard(uId.toString(),cId);
     }
     
     @RequestMapping(method=RequestMethod.GET,value="/sellCard/{cId}")
-    public void sellCard (@CookieValue(value = "id", defaultValue = "0") String uId, @PathVariable String cId) {
-		  mService.sellCard(uId, cId);	
+    public void sellCard (@PathVariable String cId) {
+    	String reqUrl = "http://127.0.0.1:8083/getAuthId";
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<Integer> bodyIu = restTemplate.getForEntity(reqUrl, Integer.class);
+        Integer uId = bodyIu.getBody();
+		mService.sellCard(uId.toString(), cId);	
     }	
 }
